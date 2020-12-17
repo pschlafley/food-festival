fc682dcdcb7a1b532451aaa9b062600cab94f61f
Peyton Schlafley <pschlafley0@gmail.com>
2020-12-16 20:42:23 -0500

    used npm packages to optimize and modularize our code to make it more DRY

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,52 @@
 const path = require('path');
 const webpack  = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: { 
+        apps: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
 
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist',
     },
+    module: {
+        rules: [
+          {
+            test: /\.jpg$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name(file) {
+                    return '[path][name].[ext]';
+                  },
+                  publicPath: function(url) {
+                    return url.replace('../', '/assets/');
+                  }
+                }
+              },
+              {
+                loader: 'image-webpack-loader'
+              }
+            ]
+          }
+        ]
+    },
+      
 
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
             'jQuery': 'jquery'
+        }),
+        new BundleAnalyzerPlugin({
+            // this will output an html file called report.html that will generate in the dist folder
+            analyzerMode: 'static'
         })
     ],
 
